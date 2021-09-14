0b7c17e05d76aa71742329c48b3069273de167da
paulwrath1223 <36117326+paulwrath1223@users.noreply.github.com>
2021-09-14 12:10:31 -0700

    Add files via upload

--- click on red.py
+++ click on red.py
@@ -1,1 +1,38 @@
-# combine other files to make the full script
+
+import win32api
+from pynput.mouse import Button, Controller
+from ctypes import windll
+
+
+from screeninfo import get_monitors
+
+
+def getpixel(x, y):   # timed to take ~ 0.01s
+    return tuple(int.to_bytes(windll.gdi32.GetPixel(dc, x, y), 3, "little"))
+
+
+dc = windll.user32.GetDC(0)
+mouse = Controller()
+
+mouseDown = True
+
+monitorX = 1920
+monitorY = 1080
+
+for m in get_monitors():
+    if m.is_primary:
+        monitorX = m.width
+        monitorY = m.height
+
+crossX = int(monitorX/2)
+crossY = int(monitorY/2)+10  # Rough estimate of crosshair location
+
+while True:  # Replace with a case for stopping program
+    if mouseDown:
+        color = getpixel(crossX, crossY)
+        if color == (255, 0, 0):  # measure the real value for real program
+            print("click")
+            break  # remove for actual code
+
+
+
