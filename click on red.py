6d08d385f79137d3324f6eb9668d6d875583bdf7
paulwrath1223 <paul@fornage.net>
2021-09-14 18:35:00 -0700

    removed hotkey

--- click on red.py
+++ click on red.py
@@ -37,14 +37,14 @@ crossX = int(monitorX/2)
 crossY = int(monitorY/2)
 
 while True:  # Replace with a case for stopping program
-    state_left = win32api.GetKeyState(135)  # Left button down = 0 or 1. Button up = -127 or -128
-    if state_left == -127 or state_left == -128:
+    # state_left = win32api.GetKeyState(135)  # Left button down = 0 or 1. Button up = -127 or -128
+    # if state_left == -127 or state_left == -128:
 
-        color = getpixel(crossX, crossY)
-        if color == (255, 0, 0):  # measure the real value for real program
+    color = getpixel(crossX, crossY)
+    if color == (255, 0, 0):  # measure the real value for real program
 
 
-            # Consider using an arduino to emulate a hardware click
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
-            # remove for actual code
+        # Consider using an arduino to emulate a hardware click
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
+        # remove for actual code
