dbcfdde06ef412e26bee5870950317928f9d4dec
paulwrath1223 <paul@fornage.net>
2021-09-16 11:34:06 -0700

    untested

--- aim assist and auto shoot.py
+++ aim assist and auto shoot.py
@@ -50,24 +50,23 @@ while True:   # Replace with a case for stopping program
     state_left = win32api.GetKeyState(135)  # Left button down = 0 or 1. Button up = -127 or -128
     if state_left == -127 or state_left == -128:  # Should this be "state_left <= -127"?
         crossColor = getpixel(crossX, crossY)
-        if redFallingEdge:
-            colorUp = (getpixel(crossX, crossY + 10))
-            colorDown = (getpixel(crossX, crossY - 10))
-            colorRight = (getpixel(crossX + 10, crossY))
-            colorLeft = (getpixel(crossX - 10, crossY))
+        colorUp = (getpixel(crossX, crossY + 10))
+        colorDown = (getpixel(crossX, crossY - 10))
+        colorRight = (getpixel(crossX + 10, crossY))
+        colorLeft = (getpixel(crossX - 10, crossY))
 
-            redRatioUp = int(colorUp[0] / sum(list(colorUp)))
-            redRatioDown = int(colorDown[0] / sum(list(colorDown)))
-            redRatioRight = int(colorRight[0] / sum(list(colorRight)))
-            redRatioLeft = int(colorLeft[0] / sum(list(colorLeft)))
+        redRatioUp = int(colorUp[0] / sum(list(colorUp)))
+        redRatioDown = int(colorDown[0] / sum(list(colorDown)))
+        redRatioRight = int(colorRight[0] / sum(list(colorRight)))
+        redRatioLeft = int(colorLeft[0] / sum(list(colorLeft)))
 
-            deltaX = strength * (redRatioRight - redRatioLeft)
-            deltaY = strength * (redRatioUp - redRatioDown)
+        deltaX = strength * (redRatioRight - redRatioLeft)
+        deltaY = strength * (redRatioUp - redRatioDown)
 
-            win32api.SetCursorPos((crossX+deltaX, crossY+deltaY))  # change mouse pos by deltaX and deltaY
+        win32api.SetCursorPos((crossX+deltaX, crossY+deltaY))  # change mouse pos by deltaX and deltaY
 
-        if red:  # measure the real value for real program
-            # Consider using an arduino to emulate a hardware click
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
-            # remove for actual code
+    if red:  # measure the real value for real program
+        # Consider using an arduino to emulate a hardware click
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
+        # remove for actual code
