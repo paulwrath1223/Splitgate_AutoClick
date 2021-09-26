a2864dc47fcac513acfdbe0b5d70477820678755
paulwrath1223 <paul@fornage.net>
2021-09-25 18:58:53 -0700

    made f24 whitelist, and removed spam click

--- click on red.py
+++ click on red.py
@@ -9,7 +9,7 @@ import win32con
 from screeninfo import get_monitors
 
 
-def getpixel(x, y):   # timed to take ~ 0.01s
+def get_pixel(x, y):   # timed to take ~ 0.01s
     return tuple(int.to_bytes(windll.gdi32.GetPixel(dc, x, y), 3, "little"))
 
 
@@ -31,24 +31,26 @@ for m in get_monitors():
 
 crossX = int(monitorX/2)
 crossY = int(monitorY/2)
-crossColor = getpixel(crossX, crossY)
+crossColor = get_pixel(crossX, crossY)
+red = False
 
 while True:  # Replace with a case for stopping program
-    if red and crossColor != (255, 0, 0):
-        redFallingEdge = True
-    else:
-        redFallingEdge = False
-
+    # if red and crossColor != (255, 0, 0):
+    #     redFallingEdge = True
+    # else:
+    #     redFallingEdge = False
+    lastRed = red
     red = (crossColor == (255, 0, 0))
-    # state_left = win32api.GetKeyState(135)  # Left button down = 0 or 1. Button up = -127 or -128
-    crossColor = getpixel(crossX, crossY)
-    # if state_left == 0 or state_left == 1:
-    if redFallingEdge:
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
-
-    if red:  # measure the real value for real program
-        # Consider using an arduino to emulate a hardware click
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
-        # remove for actual code
+    state_left = win32api.GetKeyState(135)  # Left button down = 0 or 1. Button up = -127 or -128
+    crossColor = get_pixel(crossX, crossY)
+    if state_left == -127 or state_left == -128:
+        # if redFallingEdge:
+        #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
+        #     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
+        if lastRed ^ red:
+            if red:  # measure the real value for real program
+                # Consider using an arduino to emulate a hardware click
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, crossX, crossY, 0, 0)
+            else:
+                win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, crossX, crossY, 0, 0)
+                # remove for actual code
